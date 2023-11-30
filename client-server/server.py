f762910bee36771a1ac3d2d61889b6bd1eae357d
Mark Victor Kithinji <135356007+VictorCodebase@users.noreply.github.com>
2023-11-30 16:37:07 +0300

    Add files via upload

--- client-server/server.py
+++ client-server/server.py
@@ -1,32 +1,39 @@
-
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)#create socket, family ipv4,
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)#create socket, family ipv4, type stream
 ip = socket.gethostbyname(socket.gethostname())
 allowedIP = ip
-
 port = 3000
+
 
 s.bind(('', port)) 
 s.listen(2) 
 print('Server is listening')
 
+while True:
+    # Accept incoming connections
+    conn, addr = s.accept() 
 
-conn, addr = s.accept() 
+    print('Got connection from', addr)
 
-print('Got connection from', addr)
+    if addr[0] == allowedIP:
+        print("address:", addr[0])
 
-if addr[0] == allowedIP:
-    print("address:", addr[0])
+        #receiving and decoding data
+        data = conn.recv(1024) 
+        data  = data.decode()
 
-    data = conn.recv(1024)  
-    print('Received data from client: ', data)
-    conn.send(b'Thank you for connecting')
-    conn.close()
-else:
-    print('Connection from', addr, 'refused')
-    conn.close()
 
+        print('Received data from client: ', data)
+        conn.send(b'Thank you for connecting')
+        conn.close()
+        if(data == "close"):
+            print("closing server...")
+            break
+    else:
+        print('Connection from', addr, 'refused')
+        conn.close()
 
 
 
