ed6519d81550c87a04d1433f748c658fbeacc4ab
Mark Victor Kithinji <135356007+VictorCodebase@users.noreply.github.com>
2023-11-30 16:38:36 +0300

    Add files via upload

--- client-server/client.py
+++ client-server/client.py
@@ -1,35 +1,16 @@
-'''
-client server project
-NSA
-
-Members
-Daniel Wekesa - SCT212-0183/2022
-Josiah Ndirangu - SCT212-0111/2022
-Samuel Gicheha - SCT212-0118/2022
-Ttsumah Derick Richard - SCT212-0192/2022
-Mark Victor Kithinji - SCT212-0105/2022
-
-'''
 import socket
 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-
 port = 3000
 ip = socket.gethostbyname(socket.gethostname())
 
 connectionString =(ip, port)
 s.connect(connectionString)
 
-
-
-
 message = input("Enter sample message to send to server: ")
 s.send(message.encode()) 
 
-
-print(s.recv(1024))
+print((s.recv(1024).decode()))
 
 s.close()
-
