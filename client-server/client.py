83d302e81c0cd2f7ab72bd5629cc4c565a554a86
Mark Victor Kithinji <135356007+VictorCodebase@users.noreply.github.com>
2023-11-30 14:03:35 +0300

    Add files via upload

--- client-server/client.py
+++ client-server/client.py
@@ -11,13 +11,8 @@ Mark Victor Kithinji - SCT212-0105/2022
 
 '''
 import socket
-# In Python, a socket is a low-level networking module that provides a mechanism for 
-# processes on different devices to communicate over a network.
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#s obj
-#comm ipv4
-#s_tream stream data
 
 port = 3000
 ip = socket.gethostbyname(socket.gethostname())
@@ -30,9 +25,9 @@ s.connect(connectionString)
 
 message = input("Enter sample message to send to server: ")
 s.send(message.encode()) 
-# utf8 encoding 
+
 
 print(s.recv(1024))
 
 s.close()
-#copilot
+
--- client-server/server.py
+++ client-server/server.py
@@ -1,18 +1,18 @@
 
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)#create socket, family ipv4, type stream
 ip = socket.gethostbyname(socket.gethostname())
 allowedIP = ip
 
 port = 3000
 
-s.bind(('', port))
-s.listen(2) #client per conn
+s.bind(('', port)) 
+s.listen(2) 
 print('Server is listening')
 
 
-conn, addr = s.accept() #conn -> new 
+conn, addr = s.accept() 
 
 print('Got connection from', addr)
 print("add", addr[0])
@@ -20,7 +20,7 @@ print("add", addr[0])
 if addr[0] == allowedIP:
     print("address:", addr[0])
 
-    data = conn.recv(1024)  # Receive data from the client
+    data = conn.recv(1024)  
     print('Received data from client: ', data)
     conn.send(b'Thank you for connecting')
     conn.close()
